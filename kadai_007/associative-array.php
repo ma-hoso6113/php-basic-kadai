ad35691fa48145f6b8f2c8261fdf3463b2edc25c
ma-hoso6113 <158636921+ma-hoso6113@users.noreply.github.com>
2024-05-11 15:36:37 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>php_kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+     $vegetables_data=['name'=>'onion','price'=>200,'weight'=>160];
+     print_r($vegetables_data);
+     ?>
+  </p>
+  
+</body>
+</html>
